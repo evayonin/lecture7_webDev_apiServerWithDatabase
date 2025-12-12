8edd0ce1ca59d0ebb58e7dd88031d5c27d8aeb45
Eva Yonin <eva.yonin@gmail.com>
2025-12-12 17:54:49 +0200

    הערות נוספות שלי לשל אנה

--- src/main/java/com/college/utils/DbUtils.java
+++ src/main/java/com/college/utils/DbUtils.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Component
 public class DbUtils {
-    private Connection connection;//יוצרים חיבור (קונקשן) בעצם ככה הקליינט מזדהה ואומר הנה היוזר שלי
+    private Connection connection;//יוצרים חיבור (קונקשן) בעצם ככה הקליינט מזדהה ואומר הנה היוזר שלי וככה הקליינט יוכל לבצע שאילתות
 
 
     @PostConstruct//מתודה שתרוץ מיד על העלאת האפליקציה ופה ניצור את הקונקשן
@@ -21,8 +21,10 @@ public class DbUtils {
         String username = "root";//פה אנחנו נכתוב את המידע שכתבנו כשיצרנו את הדטהבייס
         String password = "123";//וזאת הסיסמה, (אני יצרתי 123 אבל אוה אם את קוראת תעשי 1234 כמו שי שלא יהיו לך שגיאות
 
+        // כשאני אצור את הדאטה בייס ליצור אותו עם ססמה 1234 ולשנות פה ל1234
+
         //שי בדק דרך הגוגל איזה סטרינג צריך לשים כדי לחבר לדטהבייס ופשוט שינה את הדברים שלו
-        String url = "jdbc:mysql://localhost:3306/ash_2025?useSSL=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://localhost:3306/ash_2025?useSSL=false&serverTimezone=UTC"; // זו המחרוזת שאיתה פותחים את הקונקשן למסד הנתונים - עם שינוי לשם הסכימה שיצרנו ash_2025 ולשנות את הפורט אם הגדרנו משהו אחר (בכללי אם מתקינים על אותו מחשב כמה מסדי נתונים עם פורטים שונים בעת ההתקנה של הספרייה אז ניתן מספרים עוקבים לפורטים עבור כל דאטה בייס)
         try {
             this.connection = DriverManager.getConnection(url, username, password);//עוטפים את זה בטריי קאץ', וכך אנחנו מאתחלים את השדה של המחלקה
             System.out.println("Connection established!");//אם זה מוצג - זה אומר שהצלחתי ליצור קשר עם הדטה בייס
@@ -36,7 +38,7 @@ public class DbUtils {
         try {
             PreparedStatement statement = this.connection.prepareStatement(//יוצר אובייקט שנקרא פריפרד סטייטמנט (שאילתא). כדי שנוכל אחר כך לשאול את הדטה בייס שאלה והוא יוכל להחזיר תשובה
                     "INSERT INTO users (first_name, last_name, phone, username)" +
-                    "VALUE (?, ?, ?, ?)");//הסימני שאלה זה פלייסהולדרים
+                    "VALUE (?, ?, ?, ?)");//הסימני שאלה זה פלייסהולדרים (ה-?) כדי שלא יהיה הארד קודד עם ערכים קבועים
             statement.setString(1, user.getFirstName());//ככה ממלאים אותם, במיקום ה1 בתוך הסוגריים יהיה שם פרטי וכך הלאה..
             statement.setString(2, user.getLastName());//מיקום 2 שם משפחה..
             statement.setString(3, user.getPhone());
@@ -45,30 +47,30 @@ public class DbUtils {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }     // בכללי: פרפר סטייטמנט זה לא שאילתה באמת - זו פשוט בקשה שלי מהמסד נתונים. אבל ככה קוראים לזה בערית מה לעשות :/ .
 
-    }
 
+    // מה שיחזור לי בקונטרולר - אחרי שכבר חיברנו את זה לדאטה בייס שינינו שלא תחזור סתם רשימת היוזרים כמו שהיה מקודם אלא רשימת היוזרים שקיימת בטבלת היוזרים בדאטה בייס
+    // זה מה שיוצג בנתיב /all כששולחים בקשה לשרת כדי לקבל את כל היוזרים (מה שכתבנו בשיעור הקודם ומה שדיברנו עליו ממה שבחילת השיעור
     public List<User> getAllUsers () {
         List<User> users = new ArrayList<>();//רשימה ריקה
         try {
             PreparedStatement preparedStatement =
                     this.connection.prepareStatement("SELECT first_name, last_name, phone, username FROM users");
             ResultSet resultSet = preparedStatement.executeQuery();//מאחורי הקלעים זה ממומש כסוג של רשימה מקושרת
-            while (resultSet.next()) {//כל פעם מביאה לי רשומה אחת, שורה אחת מתוך הדטהבייס
+            while (resultSet.next()) {//כל פעם מביאה לי רשומה אחת, שורה אחת מתוך הדאטהבייס
                 String firstName = resultSet.getString(1);
                 String lastName = resultSet.getString(2);
                 String phone = resultSet.getString(3);
-                String username = resultSet.getString(4);
-                User user = new User(firstName, lastName, phone, username);//כל רשומה
+                String username = resultSet.getString(4); // שי הגדיר את זה כשם פרטי בשיעור כי לא חייבנו שיכניסו יוזרניים ושמנו ״״ ולכן שומר מחרוזת ריקה שזה לא חוקי בטבלאות - ???
+                User user = new User(firstName, lastName, phone, username);//כל רשומה יוצרים לה יוזר
                 users.add(user);//מוסיפים לתוך הרשימה
 
             }
         } catch (SQLException e) {//אין פה אקסקיוט אפדייט כי אני לא מעדכנת כלום.. מה שכן אני מביאה שאילתא
             throw new RuntimeException(e);
         }
-
         return users;
-
     }
 
 
